1aa27fdba0ba4c7ed15aefcbc9ff69a9bddab7cd
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2025-06-16 20:54:45 -0500

    Sort imports

--- pkg/kubernetessecrets/kubernetes_secrets_builder.go
+++ pkg/kubernetessecrets/kubernetes_secrets_builder.go
@@ -2,16 +2,13 @@ package kubernetessecrets
 
 import (
 	"context"
+	errs "errors"
 	"fmt"
-	"github.com/1Password/onepassword-operator/pkg/onepassword/model"
-
+	"reflect"
 	"regexp"
 	"strings"
 
-	"reflect"
-
-	errs "errors"
-
+	"github.com/1Password/onepassword-operator/pkg/onepassword/model"
 	"github.com/1Password/onepassword-operator/pkg/utils"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
