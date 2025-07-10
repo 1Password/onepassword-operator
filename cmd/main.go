2fc63f2204676548229af93e0d1a66a284180baf
Eddy Filip <eddy.filip@agilebits.com>
2025-07-10 19:41:53 +0200

    Switch metrics-bind-address default value back to 8080
    This ensures that the upgrade is backwards-compatible.

--- cmd/main.go
+++ cmd/main.go
@@ -99,7 +99,7 @@ func main() {
 	var secureMetrics bool
 	var enableHTTP2 bool
 	var tlsOpts []func(*tls.Config)
-	flag.StringVar(&metricsAddr, "metrics-bind-address", "0",
+	flag.StringVar(&metricsAddr, "metrics-bind-address", "8080",
 		"The address the metrics endpoint binds to. "+
 			"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081",
