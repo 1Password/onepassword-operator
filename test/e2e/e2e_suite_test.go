cf9b267eafda45748d8cd4ab5b2fdf5acd45b5fc
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2025-08-21 16:02:04 -0500

    Remove commented code

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -12,22 +12,3 @@ func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "onepassword-operator e2e suite")
 }
-
-//By("create onepassword-token secret")
-//connectToken, _ := os.LookupEnv("OP_CONNECT_TOKEN")
-//Expect(connectToken).NotTo(BeEmpty())
-//output := exec.Command("kubectl", "-n", namespace, "create", "secret", "generic", "onepassword-token", "--from-literal=token="+connectToken)
-//_, err = utils.Run(output)
-//ExpectWithOffset(1, err).NotTo(HaveOccurred())
-
-//It("Secret is updated after POOLING_INTERVAL", func() {
-//	// TODO: implement
-//})
-//
-//It("Secret with `ignore-secret` annotation is not updated", func() {
-//	// TODO: implement
-//})
-//
-//It("Deployment not auto restarts when ", func() {
-//	// TODO: implement
-//})
